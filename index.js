6791d112e1e8064b9f7dcaf2ed1824b6e8fda25b
sky <flahertysky12@gmail.com>
2021-06-16 16:14:51 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+
+document.addEventListener( "DOMContentLoaded", function () {
+    updateText()
+  } );
+  
+  
+  function updateText() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
+  
+ 
